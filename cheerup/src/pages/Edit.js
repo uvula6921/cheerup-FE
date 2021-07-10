457407f485f3efcc1fb07d58bbf0b036724b3bd2
kyungjoon.park <pkj6921@naver.com>
2021-07-10 17:07:29 +0900

    update 기능 (업데이트 모달 포함)

--- cheerup/src/pages/Edit.js
+++ cheerup/src/pages/Edit.js
@@ -1,58 +1,90 @@
-// import React, { useState } from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Modal from "@material-ui/core/Modal";
-// import Backdrop from "@material-ui/core/Backdrop";
-// import Fade from "@material-ui/core/Fade";
-// import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useRef } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators as modalActions } from "../redux/modules/updateModal";
+import { actionCreators as listActions } from "../redux/modules/articles";
+import { TextField, Button } from "@material-ui/core";
+import SaveIcon from "@material-ui/icons/Save";
 
-// const useStyles = makeStyles((theme) => ({
-//   modal: {
-//     display: "flex",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   paper: {
-//     backgroundColor: theme.palette.background.paper,
-//     border: "2px solid #000",
-//     boxShadow: theme.shadows[5],
-//     width: "356px",
-//   },
-// }));
+const modalStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    width: "356px",
+    padding: "20px",
+  },
+}));
 
-// const Edit = ({ oneDict, dictId }) => {
-//   const editModalOpen = useSelector((state) => state.dict.modalOpen);
-//   const classes = useStyles();
-//   const dispatch = useDispatch();
-//   const handleModalClose = () => {
-//     dispatch(modalOpen(false));
-//   };
+const Edit = ({ article }) => {
+  // const [wordEmpty, setWordEmpty] = useState(false);
+  const contentInput = useRef();
+  const classes = modalStyles();
+  const dispatch = useDispatch();
+  const modalOpen = useSelector((state) => state.modal.modal);
 
-//   return (
-//     <div>
-//       <Modal
-//         aria-labelledby="transition-modal-title"
-//         aria-describedby="transition-modal-description"
-//         className={classes.modal}
-//         open={editModalOpen}
-//         onClose={handleModalClose}
-//         closeAfterTransition
-//         BackdropComponent={Backdrop}
-//         BackdropProps={{
-//           timeout: 500,
-//         }}
-//       >
-//         <Fade in={editModalOpen}>
-//           <div className={classes.paper}>
-//             <Write
-//               isEdit={editModalOpen}
-//               oneDict={oneDict}
-//               dictId={dictId}
-//             ></Write>
-//           </div>
-//         </Fade>
-//       </Modal>
-//     </div>
-//   );
-// };
+  const handleModalClose = () => {
+    dispatch(modalActions.openModal(false));
+  };
 
-// export default Edit;
+  const submit = () => {
+    dispatch(
+      listActions.updateArticleSV(article.id, contentInput.current.value)
+    );
+    handleModalClose();
+  };
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={modalOpen}
+        onClose={handleModalClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={modalOpen}>
+          <div className={classes.paper}>
+            <TextField
+              label="고민"
+              placeholder="고민을 입력해주세요"
+              variant="outlined"
+              fullWidth
+              inputRef={contentInput}
+              autoComplete="off"
+              defaultValue={article.content}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              startIcon={<SaveIcon />}
+              onClick={submit}
+              fullWidth
+              style={{
+                marginTop: "30px",
+              }}
+            >
+              Save
+            </Button>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+};
+
+export default Edit;
