8c392cb4c9fd2d68973260781e7b8d815519c161
kyungjoon.park <pkj6921@naver.com>
2021-07-12 11:16:32 +0900

    좋아요 버튼

--- cheerup/src/pages/List.js
+++ cheerup/src/pages/List.js
@@ -91,11 +91,12 @@ const List = (props) => {
                 size="small"
                 aria-label="delete"
                 style={{
-                  backgroundColor: "#0B184E",
+                  backgroundColor: "#fff",
                   padding: "8px",
                   position: "absolute",
                   top: "3px",
                   right: "5px",
+                  border: "1px solid #0B184E",
                 }}
                 onClick={(e) => {
                   dispatch(listActions.deleteArticleSV(l.id));
@@ -111,7 +112,7 @@ const List = (props) => {
                   <path fill="none" d="M0 0h24v24H0z" />
                   <path
                     d="M4 8h16v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8zm2 2v10h12V10H6zm3 2h2v6H9v-6zm4 0h2v6h-2v-6zM7 5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v2h5v2H2V5h5zm2-1v1h6V4H9z"
-                    fill="rgba(255,255,255,1)"
+                    fill="#0B184E"
                   />
                 </svg>
               </IconButton>
@@ -119,11 +120,12 @@ const List = (props) => {
                 size="small"
                 aria-label="edit"
                 style={{
-                  backgroundColor: "#0B184E",
+                  backgroundColor: "#fff",
                   padding: "8px",
                   position: "absolute",
                   top: "3px",
                   right: "45px",
+                  border: "1px solid #0B184E",
                 }}
                 onClick={(e) => {
                   openModal(l.id, l.content);
@@ -139,7 +141,35 @@ const List = (props) => {
                   <path fill="none" d="M0 0h24v24H0z" />
                   <path
                     d="M5 19h1.414l9.314-9.314-1.414-1.414L5 17.586V19zm16 2H3v-4.243L16.435 3.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L9.243 19H21v2zM15.728 6.858l1.414 1.414 1.414-1.414-1.414-1.414-1.414 1.414z"
-                    fill="rgba(255,255,255,1)"
+                    fill="#0B184E"
+                  />
+                </svg>
+              </IconButton>
+              <IconButton
+                size="small"
+                aria-label="edit"
+                style={{
+                  backgroundColor: "#fff",
+                  padding: "8px",
+                  position: "absolute",
+                  top: "3px",
+                  right: "85px",
+                  border: "1px solid #0B184E",
+                }}
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="18"
+                  height="18"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M14.6 8H21a2 2 0 0 1 2 2v2.104a2 2 0 0 1-.15.762l-3.095 7.515a1 1 0 0 1-.925.619H2a1 1 0 0 1-1-1V10a1 1 0 0 1 1-1h3.482a1 1 0 0 0 .817-.423L11.752.85a.5.5 0 0 1 .632-.159l1.814.907a2.5 2.5 0 0 1 1.305 2.853L14.6 8zM7 10.588V19h11.16L21 12.104V10h-6.4a2 2 0 0 1-1.938-2.493l.903-3.548a.5.5 0 0 0-.261-.571l-.661-.33-4.71 6.672c-.25.354-.57.644-.933.858zM5 11H3v8h2v-8z"
+                    fill="#0B184E"
                   />
                 </svg>
               </IconButton>
